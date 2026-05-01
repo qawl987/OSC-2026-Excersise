4f89d73c3f90af8612c2a7f70a7958ec05a93498
qawl987 <shiny861216@gmail.com>
2026-05-01 08:14:41 +0000

    feat: ex52

--- ex52/main.c
+++ ex52/main.c
@@ -8,6 +8,13 @@ extern void video_bmp_display(unsigned int* bmp_image, int width, int height);
 #define TIME_FREQ 10000000
 int usleep(unsigned int usec) {
     // TODO: Implement this function
+    unsigned long start, now;
+    unsigned long delta = ((unsigned long)usec * TIME_FREQ) / 1000000;
+    asm volatile("rdtime %0" : "=r"(start));
+    do {
+        asm volatile("rdtime %0" : "=r"(now));
+    } while (now - start < delta);
+    return 0;
 }
 
 void display_video() {
@@ -24,5 +31,6 @@ void display_video() {
 void start_kernel() {
     uart_puts("\nStarting kernel ...\n");
     // TODO: Initialize the QEMU frame buffer device
+    video_init();
     display_video();
 }
