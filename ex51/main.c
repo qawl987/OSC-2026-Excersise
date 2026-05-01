5533556bc1a4d0012a542b05e926df69e77fcf20
qawl987 <shiny861216@gmail.com>
2026-05-01 07:37:21 +0000

    feat: ex51

--- ex51/main.c
+++ ex51/main.c
@@ -43,17 +43,9 @@ extern void switch_to(struct task_struct* prev, struct task_struct* next);
 
 void schedule() {
     // TODO: Implement this function
-    struct task_struct* queue = run_queue;
-    struct task_struct* prev = queue->next;
-    struct task_struct* cur = prev->next;
-    switch_to(prev, cur);
-    while (cur->next != queue) {
-        prev = cur;
-        cur = cur->next;
-    }
-    prev->next = cur->next;
-    cur->next = queue->next;
-    queue->next = cur;
+    struct task_struct* prev = get_current();
+    struct task_struct* next = prev->next;
+    switch_to(prev, next);
 }
 
 void idle() {
