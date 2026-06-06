3a895f51b246f5aa3cd5c75d110b5732fb46b2cb
qawl987 <shiny861216@gmail.com>
2026-06-06 06:14:00 +0000

    feat: ex71

--- ex71/tmpfs.c
+++ ex71/tmpfs.c
@@ -24,6 +24,21 @@ struct vnode_operations tmpfs_vnode_ops = {.lookup = tmpfs_lookup,
 
 struct vnode* tmpfs_create_vnode(enum fsnode_type type) {
     // TODO: Implement this function
+    struct tmpfs_vnode* inode = malloc(sizeof(struct tmpfs_vnode));
+    inode->type = type;
+    inode->name[0] = '\0';
+    for (int i = 0; i < TMPFS_MAX_DIR_ENTRY; i++)
+        inode->entry[i] = (void*)0;
+    inode->data = (void*)0;
+    inode->size = 0;
+
+    struct vnode* vnode = malloc(sizeof(struct vnode));
+    vnode->mount = (void*)0;
+    vnode->v_ops = &tmpfs_vnode_ops;
+    vnode->f_ops = &tmpfs_file_ops;
+    vnode->internal = inode;
+
+    return vnode;
 }
 
 int tmpfs_setup_mount(struct filesystem* fs, struct mount* mnt) {
@@ -46,10 +61,36 @@ int tmpfs_close(struct file* file) {
 
 int tmpfs_read(struct file* file, void* buf, size_t len) {
     // TODO: Implement this function
+    struct tmpfs_vnode* inode = file->vnode->internal;
+    if (!inode || inode->type != FS_FILE)
+        return -1;
+    if (file->f_pos >= inode->size)
+        return 0;
+    size_t available = inode->size - file->f_pos;
+    size_t len_read = (len > available) ? available : len;
+    char* start_address = inode->data + file->f_pos;
+    memcpy(buf, start_address, len_read);
+    file->f_pos += len_read;
+    return len_read;
 }
 
 int tmpfs_write(struct file* file, const void* buf, size_t len) {
     // TODO: Implement this function
+    struct tmpfs_vnode* inode = file->vnode->internal;
+    if (!inode || inode->type != FS_FILE)
+        return -1;
+    if (inode->data == (void*)0) {
+        inode->data = calloc(1, TMPFS_MAX_FILE_SIZE);
+    }
+    size_t available = TMPFS_MAX_FILE_SIZE - file->f_pos;
+    size_t len_write = (len > available) ? available : len;
+    char* start_address = inode->data + file->f_pos;
+    memcpy(start_address, buf, len_write);
+    file->f_pos += len_write;
+    if (file->f_pos > inode->size) {
+        inode->size = file->f_pos;
+    }
+    return len_write;
 }
 
 int tmpfs_lookup(struct vnode* dir_node,
@@ -71,5 +112,38 @@ int tmpfs_lookup(struct vnode* dir_node,
 int tmpfs_create(struct vnode* dir_node,
                  struct vnode** target,
                  const char* component_name) {
-    // TODO: Implement this function
+    struct tmpfs_vnode* parent_node = (struct tmpfs_vnode*)dir_node->internal;
+    if (parent_node->type != FS_DIR)
+        return -1;
+
+    // 1. 先找目錄裡有沒有空的插槽 (slot)
+    int slot = -1;
+    for (int i = 0; i < TMPFS_MAX_DIR_ENTRY; i++) {
+        if (parent_node->entry[i] == NULL) {
+            slot = i;
+            break;  // 找到第一個空的就停下來
+        }
+    }
+
+    // 目錄滿了
+    if (slot == -1)
+        return -1;
+
+    // 2. 建立新檔案的 vnode
+    struct vnode* new_vnode = tmpfs_create_vnode(FS_FILE);
+    if (!new_vnode)
+        return -1;
+
+    struct tmpfs_vnode* target_node = (struct tmpfs_vnode*)new_vnode->internal;
+
+    // 3. 設定檔名
+    strncpy(target_node->name, component_name, TMPFS_MAX_FILE_NAME - 1);
+    target_node->name[TMPFS_MAX_FILE_NAME - 1] = '\0';
+
+    // 4. 註冊到父目錄中
+    parent_node->entry[slot] = new_vnode;
+
+    // 5. 傳回給呼叫者
+    *target = new_vnode;
+    return 0;
 }
